d7e849937ca004aedad0009138c85f7cd9c590db
awesomely awesome <miaorunkun@163.com>
2020-11-30 15:19:31 +0800

    antd & UX design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import react, {Component, Fragment} from 'react';
+import 'antd/dist/antd.css';
+import {Input, Button, List} from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
+
+class App extends Component{
+
+  render(){
+    return(
+      <Fragment>
+        <div style={{'marginTop':'10px', 'marginLeft': '10px'}}>
+          <Input placeholder="Todos"  size='large' style={{'width': '300px'}}/>
+          <Button type="primary" style={{'marginLeft':'5px'}}>提交</Button>
+        </div>
+        <List
+          size="large"
+          style = {{'marginLeft': '10px', 'marginRight': '10px', 'marginTop': '10px', 'width':'300px'}}
+          header={<div>Header</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={data}
+          renderItem={item => <List.Item>{item}</List.Item>}
+        />
+      </Fragment>
+    );
+  }
 }
+
 
 export default App;
