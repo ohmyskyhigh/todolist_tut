ae2e62f8e72015cb7879655877919f733978ad95
awesomely awesome <miaorunkun@163.com>
2020-12-02 18:16:05 +0800

    ini setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,16 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {reducer} from './reducer';
+import createSagaMiddleware from 'redux-saga'
+import mySaga from './sagas'
 
+const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(
   reducer, /* preloadedState, */
+  applyMiddleware(sagaMiddleware)
 );
+
+// then run the saga
+sagaMiddleware.run(mySaga)
 
 export default store;
---
+++ src/store/sagas.js
@@ -0,0 +1,7 @@
+import {takeEvery} from 'redux-saga/effects'
+
+function* mySaga() {
+
+}
+
+export default mySaga;
